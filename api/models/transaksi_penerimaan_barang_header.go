b307fcd2561601bd31cdbe61cda7237aa1e4815e
aliterm <situsali.id@gmail.com>
2024-11-14 12:42:29 +0700

    fix: adding foreign key and reference in model transaksi

--- api/models/transaksi_penerimaan_barang_header.go
+++ api/models/transaksi_penerimaan_barang_header.go
@@ -3,10 +3,12 @@ package models
 import "time"
 
 type TransaksiPenerimaanBarangHeader struct {
-	TrxInPK      uint      `json:"trx_in_pk" gorm:"primaryKey"`
-	TrxInNo      string    `json:"trx_in_no"`
-	WhsIdf       uint      `json:"whs_idf"`
-	TrxInDate    time.Time `json:"trx_in_date" gorm:"type:date;"`
-	TrxInSuppIdf uint      `json:"trx_in_supp_idf"`
-	TrxInNotes   string    `json:"trx_in_notes"`
+	TrxInPK      uint            `json:"trx_in_pk" gorm:"primaryKey"`
+	TrxInNo      string          `json:"trx_in_no"`
+	WhsIdf       uint            `json:"whs_idf"`
+	Warehose     MasterWarehouse `json:"warehouse" gorm:"foreignKey:WhsIdf;references:WhsPK"`
+	TrxInDate    time.Time       `json:"trx_in_date" gorm:"type:date;"`
+	TrxInSuppIdf uint            `json:"trx_in_supp_idf"`
+	Supplier     MasterSupplier  `json:"supplier" gorm:"foreignKey:TrxInSuppIdf;references:SupplierPK"`
+	TrxInNotes   string          `json:"trx_in_notes"`
 }
--- api/models/transaksi_pengeluaran_barang_header.go
+++ api/models/transaksi_pengeluaran_barang_header.go
@@ -3,10 +3,12 @@ package models
 import "time"
 
 type TransaksiPengeluaranBarangHeader struct {
-	TrxOutPK      uint      `json:"trx_out_pk" gorm:"primaryKey"`
-	TrxOutNo      string    `json:"trx_out_no"`
-	WhsIdf        uint      `json:"whs_idf"`
-	TrxOutDate    time.Time `json:"trx_out_date" gorm:"type:date;"`
-	TrxOutSuppIdf uint      `json:"trx_out_supp_idf"`
-	TrxOutNotes   string    `json:"trx_out_notes"`
+	TrxOutPK      uint            `json:"trx_out_pk" gorm:"primaryKey"`
+	TrxOutNo      string          `json:"trx_out_no"`
+	WhsIdf        uint            `json:"whs_idf"`
+	Warehose      MasterWarehouse `json:"warehouse" gorm:"foreignKey:WhsIdf;references:WhsPK"`
+	TrxOutDate    time.Time       `json:"trx_out_date" gorm:"type:date;"`
+	TrxOutSuppIdf uint            `json:"trx_out_supp_idf"`
+	Supplier      MasterSupplier  `json:"supplier" gorm:"foreignKey:TrxOutSuppIdf;references:SupplierPK"`
+	TrxOutNotes   string          `json:"trx_out_notes"`
 }
