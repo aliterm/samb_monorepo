30b64234adf6d4e2e8de3a262d8597f247c34fc8
aliterm <situsali.id@gmail.com>
2024-11-14 18:38:53 +0700

    fix: update dynamic sidebar active

--- frontend/src/app/components/sidebar.tsx
+++ frontend/src/app/components/sidebar.tsx
@@ -13,22 +13,38 @@ export default function SidebarComponent() {
     <Sidebar aria-label="Sidebar with multi-level dropdown example" className="h-screen">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="/" icon={HiHome}>
+          <Sidebar.Item active={pathname === '/'} href="/" icon={HiHome}>
             Home
           </Sidebar.Item>
           <Sidebar.Collapse open={pathname.startsWith('/master')} icon={HiChartPie} label="Master Data">
-            <Sidebar.Item href="/master/product">Products</Sidebar.Item>
-            <Sidebar.Item href="/master/supplier">Supplier</Sidebar.Item>
-            <Sidebar.Item href="/master/warehouse">Warehouse</Sidebar.Item>
-            <Sidebar.Item href="/master/customer">Customer</Sidebar.Item>
+            <Sidebar.Item active={pathname === '/master/product'} href="/master/product">
+              Products
+            </Sidebar.Item>
+            <Sidebar.Item active={pathname === '/master/supplier'} href="/master/supplier">
+              Supplier
+            </Sidebar.Item>
+            <Sidebar.Item active={pathname === '/master/warehouse'} href="/master/warehouse">
+              Warehouse
+            </Sidebar.Item>
+            <Sidebar.Item active={pathname === '/master/customer'} href="/master/customer">
+              Customer
+            </Sidebar.Item>
           </Sidebar.Collapse>
-          <Sidebar.Item href="/penerimaan-barang" icon={MdFormatListBulletedAdd}>
+          <Sidebar.Item
+            active={pathname === '/penerimaan-barang'}
+            href="/penerimaan-barang"
+            icon={MdFormatListBulletedAdd}
+          >
             Penerimaan Barang
           </Sidebar.Item>
-          <Sidebar.Item href="/pengeluaran-barang" icon={MdOutlineShoppingCartCheckout}>
+          <Sidebar.Item
+            active={pathname === '/pengeluaran-barang'}
+            href="/pengeluaran-barang"
+            icon={MdOutlineShoppingCartCheckout}
+          >
             Pengeluaran Barang
           </Sidebar.Item>
-          <Sidebar.Item href="/stock" icon={BsBoxes}>
+          <Sidebar.Item active={pathname === '/stock'} href="/stock" icon={BsBoxes}>
             Stock
           </Sidebar.Item>
         </Sidebar.ItemGroup>
