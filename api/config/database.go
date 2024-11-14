0a18f6b98b3598ad4de147377cdcbe6569d0ab0d
aliterm <situsali.id@gmail.com>
2024-11-14 11:07:31 +0700

    setup dotenv and postgresql for API

---
+++ api/config/database.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable TimeZone=Asia/Jakarta"
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the database!")
+	}
+
+}
--- api/server.go
+++ api/server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"os"
+	"samb-api/config"
 	"samb-api/routers"
 
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+
 	e := echo.New()
+
 	routers.RegisterRoute(e)
-	e.Logger.Fatal(e.Start(":3030"))
+	config.ConnectDB()
+
+	e.Logger.Fatal(e.Start(host + ":" + port))
 }
